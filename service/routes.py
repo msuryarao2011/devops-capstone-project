974e806bbb4e258b2038cf09b4f2c4d18a8ae416
msuryarao2011 <159034887+msuryarao2011@users.noreply.github.com>
2024-03-24 22:30:00 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -17,19 +17,16 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
-
+    
 
 ######################################################################
 
-    ######################################################################
-    # READ AN ACCOUNT
-    ######################################################################
+######################################################################
+# READ AN ACCOUNT
+######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
+
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
