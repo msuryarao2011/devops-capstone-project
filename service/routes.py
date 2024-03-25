7e06d5796fb3f6ea8e89eeb9577dfeb5c80efbe3
msuryarao2011 <159034887+msuryarao2011@users.noreply.github.com>
2024-03-25 14:45:55 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -17,8 +17,6 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
-    
-
 ######################################################################
 
 ######################################################################
@@ -26,12 +24,10 @@ def health():
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     return account.serialize(), status.HTTP_200_OK
 # GET INDEX
 ######################################################################
@@ -76,20 +72,14 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-
     app.logger.info("Request to list Accounts")
-
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-
-
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
@@ -99,8 +89,6 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -114,12 +102,9 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
