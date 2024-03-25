820158ab872a02bd8075743582c244ac1746bd0a
msuryarao2011 <159034887+msuryarao2011@users.noreply.github.com>
2024-03-25 15:55:14 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -17,12 +17,11 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
-######################################################################
+
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
@@ -30,10 +29,10 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
+
 # GET INDEX
 ######################################################################
-
-
 @app.route("/")
 def index():
     """Root URL response"""
@@ -68,11 +67,11 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
+
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     app.logger.info("Request to list Accounts")
@@ -80,11 +79,11 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
@@ -94,14 +93,11 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
-
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
@@ -109,11 +105,11 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
